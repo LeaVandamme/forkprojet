994d4dc290a5eefba388f1ee494f63a7ae5fa92c
Ben Jaafar Imen <benjaafar@a14p7.fil.univ-lille1.fr>
2018-10-19 09:31:09 +0200

    ajout de la classe licence

---
+++ licence2.java
@@ -0,0 +1,25 @@
+package tp2;
+import java.util.*;
+public class licence2 {
+	private int numlicence;
+	
+	// non private List<logiciel> log;
+
+	
+	
+	public licence2(int numlicence) {
+		super();
+		this.numlicence = numlicence;
+		
+	}
+	public int getNumlicence() {
+		return numlicence;
+	}
+	public void setNumlicence(int numlicence) {
+		this.numlicence = numlicence;
+	}
+	
+	
+	
+
+}
